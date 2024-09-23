076a68dc09cb81182ed33579763a0e59ac92c0ee
NwachukwuSam <nwachukwusamuel123@gmail.com>
2024-09-23 11:27:50 +0100

    Hero Changes

--- src/components/general/Topnav.jsx
+++ src/components/general/Topnav.jsx
@@ -1,21 +1,21 @@
 import React from "react"
 import './Topnav.css'
-import OracleousLogo2 from '../../assets/OracleousLogo2.png'
+import OracleousLogo2 from '../../assets/OracleousLogo.png'
 function Topnav() {
 
     return (
       
-      <div className='Topnav w-full flex bg-green-950 fixed z-20'>
+      <div className='Topnav w-full flex bg-white fixed z-20 h-[75px] shadow-md shadow-black'>
         <div className="Logo">
-          <img src={OracleousLogo2} alt='Logo' className=" "/>
+          <img src={OracleousLogo2} alt='Logo' className="h-[65px] ml-[-40px] "/>
         </div>
-        <h1 class="text-white mt-[30px] text-bold">Oracleous</h1>
-        <div className="Home flex space-x-[50px] pt-[30px] text-purple-200">
-          <a href="/" className="hover:text-white">Home</a>
-          <a href="/product-page" className="hover:text-white">Products</a>
-          <a href="/Our-Story-page" className="hover:text-white">Our story</a>
-          <a href="/about-us-page" className="hover:text-white">About us</a>
-          <a href="/contact-page" className="hover:text-white">Contact</a>
+       
+        <div className="Home flex space-x-[50px] pt-[30px] text-xl text-black">
+          <a href="/" className="hover:text-green-700 hover:underline">Home</a>
+          <a href="/product-page" className="hover:text-green-700 hover:underline">Products</a>
+          <a href="/Our-Story-page" className="hover:text-green-700 hover:underline">Our story</a>
+          <a href="/about-us-page" className="hover:text-green-700 hover:underline">About us</a>
+          <a href="/contact-page" className="hover:text-green-700 hover:underline">Contact</a>
         </div>
       </div>
    
--- src/components/homepage/HomeHero.jsx
+++ src/components/homepage/HomeHero.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import Farm2 from '../../assets/Farm2.jpg'
+import Farm2 from '../../assets/tree.jpg'
 import { HomeCardData } from '../../data/HomeCardData'
 
 function HomeHero() {
   return (
     <div className="Farm2" >
-          <img src={Farm2} alt='Farm2' className="w-full h-[650px] filter brightness-50"/>
-        <h1 className=' text-4xl text-purple-200 font- ml-[450px] absolute mt-[-450px] font-serif '>Welcome to the place where <br/>natural flavour is born</h1>
+          <img src={Farm2} alt='Farm2' className="w-full h-[650px] filter brightness-30"/>
+        <h1 className=' text-5xl text-purple-200 font- ml-[300px] content-center absolute mt-[-450px] font-sans font-bold '>Producing Nigeria'a Most Affordable and Undiluted Palm Oil</h1>
         <div className='h-[3px] w-[110px] bg-lime-500 ml-[580px] mt-[-350px] absolute'></div>
 
     <div className='flex space-x-[20px] absolute top-[400px] left-[150px] '>
-      {HomeCardData.map((data, index)=>{
+      {/* {HomeCardData.map((data, index)=>{
         return(
             <div key={index} className='bg-green-950 h-[500px] w-[250px] rounded-lg'>
               <h1 className='font-bold text-2xl ml-[80px]  mt-[30px] text-purple-200 font-serif'>{data.title}</h1> 
@@ -18,7 +18,7 @@ function HomeHero() {
               <h2 className='text-purple-100 ml-[50px] mr-[50px] mt-[30px]'>{data.description}</h2>
             </div>
         )
-      })}
+      })} */}
     </div>
     </div>
     
