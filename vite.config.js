3dc78e0d9f81fcba0f4c9b64e3e164cac5df334f
NwachukwuSam <nwachukwusamuel123@gmail.com>
2024-09-27 13:04:40 +0100

    vite and remix config added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'public/dist', // Ensure this matches the remix.config.js output
+    rollupOptions: {
+      input: {
+        main: 'src/main.jsx', // Adjust according to your entry point
+      },
+    },
+  },
+});
